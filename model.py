96c05f865a4622cb5483a1e24be5bdd4fbe1c509
SudarshanM7 <97434252+SudarshanM7@users.noreply.github.com>
2022-02-06 17:19:44 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,31 +1,42 @@
+from flask import Flask, redirect, url_for, render_template, request
 import pandas as pd
-import numpy as np
-import nltk, spacy
-nltk.download('all', quiet=True)
-nlp = spacy.load("en_core_web_sm")
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import sklearn
+import json
+import requests
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Ignore the warnings
 import warnings
 warnings.filterwarnings(action='ignore')
 
-def recommend_products (uname, data, ml_model_path, rec_sys_path):
+def predict_top5():
+    uname = request.form.get("username")
+    return _predict_top5(uname)
+
+
+def _predict_top5(uname):
+    ml_model_path = 'models/ML_Model.sav'
+    rec_sys_path = 'models/Recommendation_System.csv'
+    data_path = 'data/train.csv'
     ml_model = pickle.load(open(ml_model_path, 'rb'))
-    rec_system = pickle.load(open(rec_sys_path, 'rb'))
+    rec_system = pd.read_csv(rec_sys_path, encoding='latin-1')
+    rec_system.set_index('username', inplace=True)
+    data_df = pd.read_csv(data_path, encoding='latin-1')
 
     # Recommending the top 20 products to the user.
     top_20 = rec_system.loc[uname].sort_values(ascending=False)[0:20]
 
     # Joining the train and top_20 datasets to create a new train dataset
-    train_new = pd.merge(top_20, data, left_on='prodname', right_on='prodname', how='left')
+    top_20 = pd.DataFrame(top_20).reset_index()
+    train_new = pd.merge(top_20, data_df, left_on='index', right_on='prodname', how='left')
 
     # Creating the vectorizer for the user review text for the new train dataset - 'train_new'
     rec_vector = TfidfVectorizer(min_df=2, analyzer="word", max_features=3948)
-    rec_model = rec_vector.fit_transform(list(train_new['cleaned_reviews_text']))
+    rec_model = rec_vector.fit_transform(train_new['cleaned_reviews_text'].values.astype('U'))
 
     # Predicting the sentiments for all the reviews for the top 20 recommended products for a user
-    X = pd.DataFrame(rec_model.toarray(), columns=vectorizer.get_feature_names())
+    X = pd.DataFrame(rec_model.toarray(), columns=rec_vector.get_feature_names_out())
     y_pred = ml_model.predict(X)  # Logistic Regression model is used to make predictions
     train_new['user_sentiment_pred'] = y_pred
 
@@ -36,9 +47,5 @@ def recommend_products (uname, data, ml_model_path, rec_sys_path):
     # Filtering out the top 5 products with the highest percentage of positive reviews
     top_5 = top_products.sort_values(by=['user_sentiment_pred'], ascending=False)[0:5]
     top_5.rename(columns={'user_sentiment_pred': 'positive_sentiments(%)'}, inplace=True)
-    top_5 = top_5.reset_index()
-
-    return top_5['prodname']
-
-
-
+    top_5 = top_5.reset_index()['prodname']
+    return top_5
